09203c310d32970ed784a6399544510c3d3d87e5
Gabriel Cecon Carlsen <cecon@156r.fct.unesp.br>
2023-03-15 16:59:25 -0300

    refact: new message

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         </div>
         <div className="mt-2">
           <div className="text-xl font-medium text-black">Chat</div>
-          <p className="text-slate-500">You have a new message!</p>
+          <p className="text-slate-500">New message!</p>
         </div>
         <div className="mt-4">
           <Card
