d704c14fd1c13fe3960d5a4f02db7e630675b04c
lucasgstumpf <lucasgstumpf@gmail.com>
2023-03-15 17:09:04 -0300

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,34 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
 import App from 'App'
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+    },
+
+    
+  {
+    path: "contacts",
+    element: <Contact />,
+  },
+  ]);
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+    <RouterProvider router={router} />
+
+
+);
