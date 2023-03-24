ff668c70bdba1443ce7526fabf58da7ff87fb58b
unknown <vvirginnia80@gmail.com>
2023-03-24 18:21:27 -0300

    fixing imported fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily:{
-      'sans' : ['League Spartan'];
-      'serif' : ['Sanchez', 'Playfair Display'];
+    extend: {
+      fontFamily:{
+        'sans' : ['League Spartan'];
+        'serif' : ['Sanchez', 'Playfair Display'];
+      }
     }
-    extend: {}
   },
   plugins: []
 }
