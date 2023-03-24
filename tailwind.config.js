3dc6f140cccfb419c1d502c4ef9bb3009200132d
unknown <vvirginnia80@gmail.com>
2023-03-24 18:08:34 -0300

    importing project fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily:{
+      'sans' : ['League Spartan'];
+      'serif' : ['Sanchez', 'Playfair Display'];
+    }
     extend: {}
   },
   plugins: []
