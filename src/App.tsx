378489eef2b65311a21ca6da67ab101d115fbd40
Gabriel Cecon Carlsen <cecon@86r.fct.unesp.br>
2023-04-10 17:34:47 -0300

    feat: theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import Avatar from 'components/Avatar'
-import Card from 'components/Card'
+import Card from "components/Card";
+
+import { Alert, AlertIcon, AlertTitle, Box } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="bg-red-100 min-h-screen items-center justify-center h-screen flex">
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex-row items-center space-x-4">
-        <div>
-          <Avatar />
-        </div>
+        <Box margin="8">
+          <Alert status="error">
+            <AlertIcon />
+            <AlertTitle>Esse é um alerta do Chakra !</AlertTitle>
+          </Alert>
+          <Alert status="success">
+            <AlertIcon />
+            <AlertTitle>Esse é um alerta do Chakra !</AlertTitle>
+          </Alert>
+        </Box>
         <div className="mt-2">
           <div className="text-xl font-medium text-black">Chat</div>
           <p className="text-slate-500">You have a new message!</p>
@@ -21,7 +29,7 @@ function App() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
